a856c410e41f88954ee942184094c60346211107
BADRobin <olegbryl@gmail.com>
2018-08-13 20:29:17 +0300

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Main {
+    public final static double EPS = 1e-6;
+
+    private double a = 2.8;
+    private double b = -0.3;
+    private double c = 4.0;
+    public static void main(String[] args) {
+
+    }
+    public double f(double x){
+        if (x< 1.4-EPS){
+            return a*x*x+b*x+c;
+        }else if (x>1.4+EPS){
+            return (a+b*x)/Math.sqrt(x*x+1);
+        }else {
+            return a/x+Math.sqrt(x*x+1);
+        }
+    }
+    public int steps(double start, double finish, double step){
+        return (int) ((finish-start)/step+1);
+    }
+    public double[] fillX(double start, double finish, double step) {
+        return IntStream
+                .iterate(0, i->i+1)
+                .mapToDouble(i->start+i*step)
+                .limit(steps(start,finish,step))
+                .toArray();
+    }
+
+    public double[] fillY(double[] x) {
+        return Arrays.stream(x)
+                .map(this::f)
+                .toArray();
+    }
+
+    public double sum(double[] arr) {
+        return Arrays.stream(arr)
+                .reduce(0, (a,b) -> a+b);
+    }
+
+    public double numMin(double[] arr) {
+        int res = 0;
+        for (int i = 1; i <arr.length; i++) {
+            if (arr[i] < arr[res]) {
+                res = i;
+            }
+        }
+        return res;
+    }
+}
+//Додати до створеного класу метод main, перетворивши, таким чином, його на
+//автономну програму. Cкомпілювати і виконати програму
---
+++ test/main/MainTest.java
@@ -0,0 +1,93 @@
+package main;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    private final static double EPS = 1e-6;
+
+    Main main;
+
+    @BeforeEach
+    void setUp() {
+        main = new Main();
+    }
+
+    @Test
+    void testF1() {
+        double x = 1;
+        double expected = 6.5;
+        double result = main.f(x);
+        assertEquals(expected, result, EPS);
+    }
+
+    @Test
+    void testF2() {
+        double x = 2;
+        double expected = 0.98386991;
+        double result = main.f(x);
+        assertEquals(expected, result, EPS);
+    }
+
+    @Test
+    void testF3() {
+        double x = 1.4;
+        double result = main.f(x);
+        double expected = 3.720465053;
+        assertEquals(expected, result, EPS);
+    }
+
+    @Test
+    void testSteps() {
+        double start = 0;
+        double finish = 10;
+        double step = 0.1;
+        int expected = 101;
+        int result = main.steps(start, finish, step);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFillX() {
+        double start = 0;
+        double finish = 2;
+        double step = 0.002;
+        double[] expected = {0, 1.4, 2};
+        double[] result = main.fillX(start, finish, step);
+        assertEquals(expected[0], result[0], EPS);
+        assertEquals(expected[1], result[700], EPS);
+        assertEquals(expected[2], result[1000], EPS);
+    }
+
+    @Test
+    void testFillY() {
+        double start = 0;
+        double finish = 2;
+        double step = 0.002;
+        double[] x = main.fillX(start, finish, step);
+        double[] expected = {4.0, 3.720465053, 0.98386991};
+        double[] result = main.fillY(x);
+        assertEquals(expected[0], result[0], EPS);
+        assertEquals(expected[1], result[700], EPS);
+        assertEquals(expected[2], result[1000], EPS);
+    }
+
+    @Test
+    void testSum() {
+        double[] arr = {1.0, -2.1, 7.5, 4.3, 11.6};
+        double expected = 22.3;
+        double result = main.sum(arr);
+        assertEquals(expected, result, EPS);
+    }
+
+    @Test
+    void testMin() {
+        double[] arr = {1.0, -2.1, 7.5, 4.3, 11.6};
+        int expected = 1;
+        double result = main.numMin(arr);
+        assertEquals(expected,result);
+    }
+}
